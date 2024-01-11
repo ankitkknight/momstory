6812a10e40e4c1f21be6b62b3d57cc648df5b2a5
Ankit kumar <ankitkumarcode24@gmail.com>
2024-01-12 00:08:06 +0530

    deploy4

--- middleware/fetchUser.js
+++ middleware/fetchUser.js
@@ -9,7 +9,7 @@ const fetchUser = (req, res, next) => {
         res.status(401).send({ error: "Unauthorized access" })
     }
     try {
-        const data = jwt.verify(token, JWT_SECRET);
+        // const data = jwt.verify(token, JWT_SECRET);
         req.user = data.user;
         next();
     } catch (error) {
