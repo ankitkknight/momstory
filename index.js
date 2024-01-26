5bf04e82421dd9aa9b5fd9699a843c0706786222
Ankit kumar <ankitkumarcode24@gmail.com>
2024-01-26 14:43:13 +0530

    deploy18

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = 4000
 
 app.use(cookieParser());
 app.use(cors({
-    origin: ["https://65b3757a6283c3a041428e64--spectacular-lolly-85f15a.netlify.app"],
+    origin: ["https://65b3757a6283c3a041428e64--spectacular-lolly-85f15a.netlify.app", "https://spectacular-lolly-85f15a.netlify.app"],
     methods: "GET,POST,PUT,DELETE,OPTIONS",
 }))
 app.use(bodyParser.json({ limit: '50mb' }));
